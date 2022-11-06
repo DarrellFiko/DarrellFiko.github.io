d0e3c4f26fd2cf53a07a5b681ff914300a88a6ca
DarrellFiko <darrellfikoalexander@gmail.com>
2022-11-06 22:43:48 +0700

    test

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sport Station</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="bg-dark text-white fixed-top">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-2 col-lg-2 d-flex align-items-center">
+                <a class="navbar-brand" href="#">
+                <img src="asset/logo_toko.png" alt="Logo" width="50" height="50" class="d-inline-block">
+                </a>
+                <div class="navbar-brand d-lg-inline d-none fs-5">
+                Sport Station
+                </div>
+            </div>
+            <div class="col-7 col-lg-8 py-3">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-light" type="submit">Search</button>
+                </form>
+            </div>
+            <div class="col-3 col-lg-2 d-flex justify-content-end py-3">
+                <a class="navbar-brand mx-2" href="#">
+                    <img src="asset/keranjang.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+                </a>
+                <a class="navbar-brand mx-2" href="#">
+                    <img src="asset/login.png" alt="Logo" width="35" height="35" class="d-inline-block align-text-top">
+                </a>
+            </div>
+        </div>
+    </div>
+    </nav>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script> -->
+  </body>
+</html>
