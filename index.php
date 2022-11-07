dd379ffc662edd306d5dd44f3fdadc460a8d232d
Geovann Chandra <103976069+gechdr@users.noreply.github.com>
2022-11-07 10:47:34 +0700

    Update

--- index.php
+++ index.php
@@ -4,49 +4,53 @@
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sport Station</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
+</head>
+
+<body>
     <!-- NAVBAR -->
     <nav class="bg-dark text-white fixed-top">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-2 col-lg-2 d-flex align-items-center">
-                <a class="navbar-brand" href="#">
-                <img src="asset/logo_toko.png" alt="Logo" width="50" height="50" class="d-inline-block">
-                </a>
-                <div class="navbar-brand d-lg-inline d-none fs-5">
-                Sport Station
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-2 col-lg-2 d-flex align-items-center">
+                    <a class="navbar-brand" href="#">
+                        <img src="asset/logo_toko.png" alt="Logo" width="50" height="50" class="d-inline-block">
+                    </a>
+                    <div class="navbar-brand d-lg-inline d-none fs-5">
+                        Sport Station
+                    </div>
+                </div>
+                <div class="col-7 col-lg-8 py-3">
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-light" type="submit">Search</button>
+                    </form>
+                </div>
+                <div class="col-3 col-lg-2 d-flex justify-content-end py-3">
+                    <a class="navbar-brand mx-2" href="#">
+                        <img src="asset/keranjang.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+                    </a>
+                    <a class="navbar-brand mx-2" href="login.php">
+                        <img src="asset/login.png" alt="Logo" width="35" height="35" class="d-inline-block align-text-top">
+                    </a>
                 </div>
             </div>
-            <div class="col-7 col-lg-8 py-3">
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-light" type="submit">Search</button>
-                </form>
-            </div>
-            <div class="col-3 col-lg-2 d-flex justify-content-end py-3">
-                <a class="navbar-brand mx-2" href="#">
-                    <img src="asset/keranjang.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
-                </a>
-                <a class="navbar-brand mx-2" href="login.php">
-                    <img src="asset/login.png" alt="Logo" width="35" height="35" class="d-inline-block align-text-top">
-                </a>
-            </div>
         </div>
-    </div>
     </nav>
 
     <!-- SCROLL -->
     <div style="margin-top:70px">
         <!-- Div kiri buat scroll -->
         <div class="scrollKiri d-flex align-items-center justify-content-center">
-            <button class="buttonCategories d-flex align-items-center justify-content-center text-white ps-2" onclick="openNav()"><h2><img src="asset/arrow_kanan.png" alt="" width="30" height="30"></h2></button>
+            <button class="buttonCategories d-flex align-items-center justify-content-center text-white ps-2" onclick="openNav()">
+                <h2><img src="asset/arrow_kanan.png" alt="" width="30" height="30"></h2>
+            </button>
         </div>
 
         <!-- kategori -->
@@ -98,25 +102,24 @@
     <div class="text-center">
         <div class="row">
             <div class="col-2 bg-info"></div>
-            <div class="col-9 bg-danger">
-                <div class="row d-flex justify-content-end"></div>
+            <div class="col-10 bg-danger">
+                <div class="row d-flex justify-content-end">a</div>
                 <?php
-                for ($i=0; $i < 30; $i++) { 
-                    ?>
-                    <div class="col-5 col-md-4 col-xl-3">
+                for ($i = 0; $i < 30; $i++) {
+                ?>
+                    <div class="col-5 col-md-4 col-lg-3">
                         <div class="card" style="width: 15rem;">
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div></div>
+                            </div>
                         </div>
                     </div>
-                    <?php
+                <?php
                 }
                 ?>
-                </div>
             </div>
         </div>
     </div>
@@ -133,5 +136,6 @@
             document.getElementById("mySidenav").style.width = "0";
         }
     </script>
-  </body>
+</body>
+
 </html>
