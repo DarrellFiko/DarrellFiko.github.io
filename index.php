b3487f51ed0bd034dabe47558f58e467e042c7e5
Geovann Chandra <103976069+gechdr@users.noreply.github.com>
2022-11-10 10:20:27 +0700

    test

--- index.php
+++ index.php
@@ -484,7 +484,7 @@ if (isset($_POST["detail"])) {
         </div>
     </div>
 
-
+    <!-- test -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
     <!-- script buat open scroll yang dikiri -->
